6608f6da62fa7837d1725608b72194955d8dd5b7
u230042 <u230042@sbb.ch>
2021-01-26 17:52:43 +0100

    current state

--- P02-VehiclesAPI/pricing-service/src/main/java/com/udacity/pricing/domain/price/Price.java
+++ P02-VehiclesAPI/pricing-service/src/main/java/com/udacity/pricing/domain/price/Price.java
@@ -5,6 +5,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.math.BigDecimal;
+import java.util.Objects;
 
 /**
  * Represents the price of a given vehicle, including currency.
@@ -51,5 +52,20 @@ public class Price {
 
     public void setVehicleId(Long vehicleId) {
         this.vehicleId = vehicleId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Price price1 = (Price) o;
+        return Objects.equals(currency, price1.currency) &&
+                Objects.equals(price, price1.price) &&
+                Objects.equals(vehicleId, price1.vehicleId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(currency, price, vehicleId);
     }
 }
--- P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
+++ P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
@@ -1,16 +1,38 @@
 package com.udacity.pricing;
 
+import com.udacity.pricing.domain.price.Price;
+import com.udacity.pricing.repository.PriceRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class PricingServiceApplicationTests {
 
+	@Autowired
+	PriceRepository priceRepository;
+
 	@Test
-	public void contextLoads() {
+	public void testDBElementCount() {
+		Iterable<Price> prices = priceRepository.findAll();
+		Price price = new Price("USD", new BigDecimal(82735.36), 1L);
+
+		assertThat(prices).isNotEmpty().hasSize(10);
+		assertThat(prices).extracting("vehicleId").contains(1L,2L,3L,4L,5L,6L,7L,8L,9L);
+	}
+
+	@Test
+	public void testGetPriceById() {
+		ResponseEntity<Price> responseEntity = null;
 	}
 
 }
